7a570e87b5cb3a74bed2e06f2ef3afa101a06d03
huyhieublock <huy.hieu@blockodyssey.io>
2025-04-28 11:26:04 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("dev")); // HTTP request logger middleware
+app.use(helmet()); // sets various HTTP headers to help protect your app from common web vulnerabilities
+app.use(compression()); // gzip compression middleware
+
+// init db
+
+// init routes
+
+// init error handlers
+
+module.exports = app;
